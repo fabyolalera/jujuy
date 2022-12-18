d31c0297f880c944a512a2b7cfd086db7e35994a
fabyolalera <fabyolalera@gmail.com>
2022-12-18 00:49:00 -0300

    Se continua practicando

--- cac/urls.py
+++ cac/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .import views
+from django.urls import path, re_path
+
+from . import views
 
 urlpatterns = [
+    path('',views.index,name='inicio'),
     path('hola_mundo',views.hola_mundo),
+    path('saludar/<str:nombre>',views.saludar,name="saludar"),
+    path('cursos/detalle/<slug:nombre_curso>',views.cursos_detalle, name="curso_detalle"),
+    re_path(r'^cursos/(?P<nombre>\w+)/$',views.cursos,name="cursos")
 ]
--- cac/views.py
+++ cac/views.py
@@ -1,7 +1,37 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    if(request.method=='GET'):
+        titulo = 'Titulo cuando se accede por GET'
+    else:
+        titulo = f'Titulo cuando accedo por otro metodo {request.method}'
+    parameters_get = request.GET.get('otro')
+    return HttpResponse(f"""
+        <h1>{titulo}</h1>
+        <p>{parameters_get}</p>
+    """)
+
+
 def hola_mundo(request):
     return HttpResponse('Hola mundo Django')
+
+def saludar(request,nombre):
+    return HttpResponse(f"""
+        <h1>Hola Mundo - {nombre}</h1>
+        <p>Esto es una prueba</p>
+    """)
+    
+def cursos_detalle(request,nombre_curso):
+    return HttpResponse(f"""
+        <h1>{nombre_curso}</h1>
+    """)
+
+
+def cursos(request,nombre):
+    return HttpResponse(f"""
+        <h2>{nombre}</h2>
+    """)
